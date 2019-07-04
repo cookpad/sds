eb6bfe6a3a62d6aed1b82d43cd7f1bd8fbe399f0
Kohei Suzuki <kohei-suzuki@cookpad.com>
2019-07-04 17:55:38 +0900

    Fix clippy::map_entry
    usage of `contains_key` followed by `insert` on a `HashMap`
    https://rust-lang-nursery.github.io/rust-clippy/master/index.html#map_entry

--- src/v2xds.rs
+++ src/v2xds.rs
@@ -99,12 +99,13 @@ pub fn hosts_to_locality_lb_endpoints(mut hosts: Vec<Host>) -> Vec<LocalityLbEnd
         };
         let le = convert_host_to_le(h);
 
-        if lle_map.contains_key(&locality) {
-            let le_vec = lle_map.get_mut(&locality).expect("map key error");
-            le_vec.push(le);
-        } else {
-            let le_vec = vec![le];
-            lle_map.insert(locality, le_vec);
+        match lle_map.entry(locality) {
+            std::collections::hash_map::Entry::Vacant(e) => {
+                e.insert(vec![le]);
+            }
+            std::collections::hash_map::Entry::Occupied(mut e) => {
+                e.get_mut().push(le);
+            }
         }
     }
 
